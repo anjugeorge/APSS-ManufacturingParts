2ca60c22b095fea5456b95d8c7351153dbc6bf18
Anju George <anjugeorgeantony@gmail.com>
2025-01-30 21:54:29 -0500

    Changed styles

--- src/componenets/Pages/Process/ProcessHero.jsx
+++ src/componenets/Pages/Process/ProcessHero.jsx
@@ -9,34 +9,33 @@ const ProcessHero = () => {
   return (
     <>
       <main>
-        <div className=" relative overflow-hidden">
-          <div className="container   grid grid-cols-1 md:grid-cols-2  pt-10">
-            <div className=" text-black  text-center md:text-start  ">
-              {" "}
-              <h1 className="lg:text-4xl md:text-2xl text-lg pb-3 font-bold">
+        <div>
+          <div className="container grid grid-cols-1 md:grid-cols-3 bg-[#020202] md:py-16">
+            <div className="flex flex-col col-span-2 py-10 md:py-10 text-center md:text-start ">
+              <h1 className="lg:text-5xl  md:text-4xl sm:text-3xl pb-3  text-white">
                 Get Your Tailored Quote Today!
               </h1>
-              <p className="pb-3  text-sm/7 ">
+              <p className="pb-3 md:w-5/6 text-sm/7 text-stone-300">
                 At APSS, we are your one-stop shop for all things mechanical.
                 Whether you need industrial machinery parts, general mechanical
                 spares, or automotive components, we have got you covered.
               </p>
               <div className="pb-5">
+                {" "}
                 <FormDialog />
               </div>
             </div>
             <div
-              className="mx-auto  md:pt-28"
+              className="mx-auto"
               data-aos="zoom-in"
               data-aos-duration="1500"
             >
               <img
-                className="h-auto rounded-lg w-full max-w-xs  md:pt-20"
-                src="/assets/header.png"
+                className="h-auto rounded-lg w-full max-w-lg"
+                src="/assets/679babb450e29.png"
                 alt="image description"
               />
             </div>
-            <div class="bg-black w-[2500px] h-[2500px]  rounded-3xl absolute top-[15%] left-[-10%] md:top-[30%] md:left-[10%] rotate-[77deg] -z-10"></div>
           </div>
         </div>
       </main>
