e3c45fb7c2a0c2360a5c16fb48fedbe5f1754aed
Anju George <anjugeorgeantony@gmail.com>
2025-01-31 20:14:20 -0500

    Changed button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
       container: {
         center: true, // Centers the container
         padding: {
@@ -15,8 +24,8 @@ export default {
       },
       colors: {
         headerColor: "#000000",
-        buttonColor: "#A87676",
-        buttonHoverColor: "#7D5A5A",
+        buttonColor: "#3D606E",
+        buttonHoverColor: "#153B47",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
